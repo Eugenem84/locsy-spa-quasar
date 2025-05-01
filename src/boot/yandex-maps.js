fd4d62260ff1ffdb29d4a42623df709d9d92eb6d
Eugenem84 <medov-foto@mail.ru>
2025-05-01 09:35:29 +0300

    попутка перехода на vue-yandex-maps v3, не работает

--- src/boot/yandex-maps.js
+++ src/boot/yandex-maps.js
@@ -1,14 +1,8 @@
-import { boot } from 'quasar/wrappers'
-//import YandexMaps from 'vue-yandex-maps'
-import { createYmaps} from 'vue-yandex-maps'
+import { createYmaps } from 'vue-yandex-maps'
 
-export default boot(async ({ app }) => {
-  const yandexMap = createYmaps({
+export default ({ app }) => {
+  app.use(createYmaps({
     apikey: '2e40d235-9d0b-4f1f-bc61-4a6a4c5cafec',
     lang: 'ru_RU',
-    version: '2.1',
-    //initialization: 'mounted'
-  })
-
-  app.use(yandexMap)
-})
+  }))
+}
