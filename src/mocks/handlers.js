dd36a99ef83e8a9144a2efd5ffcd22ef7f14b156
Eugenem84 <medov-foto@mail.ru>
2025-05-16 11:23:26 +0300

    реализовал описание локаций

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,12 +1,34 @@
 import { http, HttpResponse } from 'msw'
 
 const allLocations = [
-  { id: 1, name: 'Парк Горького', city_id: 1, lat: 55.729876, lng: 37.603493, rating: 4.7 },
-  { id: 2, name: 'ВДНХ', city_id: 1, lat: 55.829895, lng: 37.633266, rating: 4.6 },
-  { id: 3, name: 'Елагин остров', city_id: 2, lat: 59.972218, lng: 30.217171, rating: 4.9 },
+  { id: 1,
+    name: 'Парк Горького',
+    city_id: 1,
+    lat: 55.729876,
+    lng: 37.603493,
+    rating: 4.7,
+    description: 'Большой парк в центре Москвы с набережной, прокатом велосипедов и культурными мероприятиями.'
+  },
+  { id: 2,
+    name: 'ВДНХ',
+    city_id: 1,
+    lat: 55.829895,
+    lng: 37.633266,
+    rating: 4.6,
+    description: 'Выставочный комплекс с павильонами, фонтанами, парками и музеями.'
+  },
+  { id: 3,
+    name: 'Елагин остров',
+    city_id: 2,
+    lat: 59.972218,
+    lng: 30.217171,
+    rating: 4.9,
+    description: 'Зелёный остров в Петербурге с дворцом, мостами и тихими прогулочными маршрутами.'
+  },
 ]
 
 export const handlers = [
+
   http.get('/locations', ({ request }) => {
     const url = new URL(request.url)
     const cityIdParam = url.searchParams.get('city_id')
@@ -17,5 +39,18 @@ export const handlers = [
       : allLocations
 
     return HttpResponse.json(filteredLocations, { status: 200 })
+  }),
+
+  http.get('/location/:id', ({ params }) => {
+    const id = parseInt(params.id)
+    const location = allLocations.find(location => location.id === id)
+
+    if (!location){
+      return HttpResponse.json({ message: 'Location not found' }, {status: 404})
+    }
+    //console.log('location: ', location)
+
+    return HttpResponse.json(location, {status: 200})
   })
+
 ]
--- src/pages/LocationPage.vue
+++ src/pages/LocationPage.vue
@@ -1,9 +1,32 @@
 <script setup>
+import { onMounted, ref } from 'vue'
+import {useRoute} from 'vue-router'
+import { api } from 'boot/axios.js'
+
+const route = useRoute()
+const id = route.params.id
+const location = ref(null)
+
+onMounted(async () => {
+  const response = await api(`http://localhost:9000/location/${id}`)
+  location.value = await response
+  console.log('location: ', location)
+})
 
 </script>
 
 <template>
-не реализовано
+
+  <q-page class="q-pa-md">
+    <h4> локация #{{id}}</h4>
+    <div v-if="location">
+      <p><strong>Название: </strong> {{location.data.name }}</p>
+      <p><strong>Описание: </strong> {{location.data.description}}</p>
+    </div>
+    <div v-else>
+      <q-spinner color="primary" />
+    </div>
+  </q-page>
 </template>
 
 <style scoped>
